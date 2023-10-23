257d278cccf8f9ae5075a3b1482f128dc844b27e
Владислав Коровин <skyline_blade@mail.ru>
2023-10-23 06:57:29 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import React,{useState} from 'react';
 
 function App() {
+
+  const[step, setStep]=useState('');
+
   return (
     <div className="App">
       <h1>Tic-Tac-Toe application</h1>
+      <Board/>
     </div>
   );
 }
